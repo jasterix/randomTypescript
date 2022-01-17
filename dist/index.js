6bd36e4efbbaeccbffc7107c655505294fdf6c6c
Jasterix <jasterix19@gmail.com>
2022-01-17 12:34:56 -0500

    Update folder structure

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
 let id = 5;
+console.log("ID:", id);
+//enum
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "Up";
+    Direction1["Down"] = "Down";
+    Direction1["Left"] = "Left";
+    Direction1["Right"] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Down);
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let id:number =5;
+
+console.log("ID:", id)
+
+//enum
+enum Direction1 {
+    Up = "Up",
+    Down = "Down",
+    Left="Left",
+    Right = "Right",
+    }
+
+    console.log(Direction1.Down)
